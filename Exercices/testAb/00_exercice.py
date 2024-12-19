77c1102659b9e3428f60ab8aac47ac3fbf1a764a
Antoine07 <antoine.lucsko@gmail.com>
2024-12-19 09:33:20 +0100

    test AB

---
+++ Exercices/testAb/00_exercice.py
@@ -0,0 +1,39 @@
+import random
+import matplotlib.pyplot as plt
+
+# Simuler les visiteurs
+TOTAL_VISITORS = 1_000
+
+def simulate_button_clicks(total_visitors, prob_click_A, prob_click_B):
+    """
+    Simule les clics des visiteurs sur deux versions de boutons.
+
+    :param total_visitors: Nombre total de visiteurs
+    :param prob_click_A: Probabilité de clic pour le bouton A
+    :param prob_click_B: Probabilité de clic pour le bouton B
+    :return: Nombre de clics pour A et B
+    """
+    # SIMULATION
+
+    return clicks_A, clicks_B
+
+# Définir les probabilités de clic
+
+
+# Simuler les clics
+
+
+# Afficher les résultats
+print(f"Version A (Acheter maintenant) :  clics")
+print(f"Version B (Ajouter au panier) :  clics")
+
+# Créer le graphique
+labels = ['Version A (Acheter maintenant)', 'Version B (Ajouter au panier)']
+values = [] # nombre de clicks A et B
+colors = ['blue', 'red']
+
+plt.bar(labels, values, color=colors)
+plt.title('Comparaison des clics sur les boutons')
+plt.ylabel('Nombre de clics')
+plt.xlabel('Version du bouton')
+plt.show()
