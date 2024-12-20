55dab8aab527c5762f6e22ba21a3e59568e2258f
Antoine <antoine.lucsko@wanadoo.fr>
2024-12-20 08:57:53 +0100

    Update Product.py

--- Corrections/CoffeeShop/Models/Product.py
+++ Corrections/CoffeeShop/Models/Product.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-@dataclass(eq=True, frozen=True)  # Ajoute immutabilité et support pour hashing
+@dataclass(eq=True, frozen=True)  # Ajoute immutabilité et support pour hashing permet de définir des clés dans les dictionnaires , un objet unique une clé unique
 class Product:
     name: str
     price: float
